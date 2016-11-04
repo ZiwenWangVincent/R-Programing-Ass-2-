bc632c58d070a9eddcaf2ce2e9a6be316e70d2fa
ZiwenWangVincent <13102636d@connect.polyu.hk>
2016-11-04 21:00:16 +0800

    Update code.r

--- code.r
+++ code.r
@@ -1,3 +1,5 @@
+## Vincent Wang 
+## This is my programming assignment 2
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -20,10 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         inv <- x$getInverse()
         if (!is.null(inv)) {
-                message("getting cached data")
+                message("getting data")
                 return(inv)
         }
         mat <- x$get()
